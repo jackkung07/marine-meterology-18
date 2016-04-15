ee6e09435c9b8abcd471ca7f1922b849f7872e28
jackkung07 <jackkung07@gmail.com>
2016-04-14 20:23:26 -0700

    init commit

---
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,21 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String mainPage(ModelMap model) {
+		//model.addAttribute("message", "Hello world!");
+		return "main";
+	}
+
+	@RequestMapping(value = "/sensormgn", method = RequestMethod.GET)
+	public String sensorMan(ModelMap model) {
+		//model.addAttribute("message", "Hello world!");
+		return "sensorMgn";
+	}
+}
