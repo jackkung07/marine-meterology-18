3f781dd2f86b6b7d51aeb50118a64d885120b222
ivanybma <ivanybma@ivanybmas-MacBook-Pro.local>
2016-05-13 22:31:28 -0700

    added sensor type and location enum

---
+++ src/main/java/com/springapp/mvc/sensorEntity/SDataEntity.java
@@ -0,0 +1,64 @@
+package com.springapp.mvc.sensorEntity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * Created by ivanybma on 5/13/16.
+ */
+@Document(collection = "sensordata")
+public class SDataEntity {
+
+    @Id
+    private String Id;
+    String sensorType;
+    String sensorLocation;
+    String dataDateTime;
+    String dataValue;
+
+    @PersistenceConstructor
+    public SDataEntity(String sensorType, String sensorLocation, String dataDateTime, String dataValue) {
+        super();
+        this.sensorType = sensorType;
+        this.sensorLocation = sensorLocation;
+        this.dataDateTime = dataDateTime;
+        this.dataValue = dataValue;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    public String getSensorLocation() {
+        return sensorLocation;
+    }
+
+    public void setSensorLocation(String sensorLocation) {
+        this.sensorLocation = sensorLocation;
+    }
+
+    public String getDataDateTime() {
+        return dataDateTime;
+    }
+
+    public void setDataDateTime(String dataDateTime) {
+        this.dataDateTime = dataDateTime;
+    }
+
+    public String getDataValue() {
+        return dataValue;
+    }
+
+    public void setDataValue(String dataValue) {
+        this.dataValue = dataValue;
+    }
+
+
+}
